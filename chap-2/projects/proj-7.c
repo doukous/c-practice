8fc818e8242d0f16725231bab28174d2f53f08ce
Mohamed Doucouré <mohamed.doucoure@protonmail.com>
2025-10-24 19:54:57 +0000

    small edit for a more concise code

--- chap-2/projects/proj-7.c
+++ chap-2/projects/proj-7.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 int main(void)
 {
     int amount;
@@ -8,13 +7,13 @@ int main(void)
     scanf("%d", &amount);
 
     printf("$20 bills: %d \n", amount / 20);
-    amount = amount - (amount / 20) * 20;
+    amount -= (amount / 20) * 20;
 
     printf("$10 bills: %d \n", amount / 10);
-    amount = amount - (amount / 10) * 10;
+    amount -= (amount / 10) * 10;
 
     printf("$5 bills: %d \n", amount / 5);
-    amount = amount - (amount / 5) * 5;
+    amount -= (amount / 5) * 5;
 
     printf("$1 bills: %d \n", amount);
 }
