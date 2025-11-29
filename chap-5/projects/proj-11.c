4a6a1b87940069b39bfd266c41c86758dc0f94ce
Mohamed Doucouré <mohamed.doucoure@protonmail.com>
2025-11-29 21:13:44 +0000

    improve a little the code

--- chap-5/projects/proj-11.c
+++ chap-5/projects/proj-11.c
@@ -3,82 +3,84 @@
 int main(void)
 {
     int number;
-    
+
     printf("Enter a two-digit number: ");
     scanf("%d", &number);
+
+    printf("You entered the number ");
 
     switch (number / 10)
     {
     case 9:
-        printf("You entered the number ninety-");
+        printf("ninety-");
         break;
 
     case 8:
-        printf("You entered the number eighty-");
+        printf("eighty-");
         break;
 
     case 7:
-        printf("You entered the number seventy-");
+        printf("seventy-");
         break;
 
     case 6:
-        printf("You entered the number sixty-");
+        printf("sixty-");
         break;
 
     case 5:
-        printf("You entered the number fifty-");
+        printf("fifty-");
         break;
 
     case 4:
-        printf("You entered the number forty-");
+        printf("forty-");
         break;
 
     case 3:
-        printf("You entered the number thirty-");
+        printf("thirty-");
         break;
 
     case 2:
-        printf("You entered the number twenty-");
+        printf("twenty-");
         break;
 
     case 19:
-        printf("You entered the number nineteen.");
+        printf("nineteen.");
         break;
 
     case 18:
-        printf("You entered the number eighteen.");
+        printf("eighteen.");
         break;
-    
+
     case 17:
-        printf("You entered the number seventeen.");
+        printf("seventeen.");
         break;
-    
+
     case 16:
-        printf("You entered the number sixteen.");
+        printf("sixteen.");
         break;
-    
+
     case 15:
-        printf("You entered the number fifteen.");
+        printf("fifteen.");
         break;
 
     case 14:
-        printf("You entered the number fourteen.");
+        printf("fourteen.");
         break;
-    
+
     case 13:
-        printf("You entered the number thirteen.");
+        printf("thirteen.");
         break;
-    
+
     case 12:
-        printf("You entered the number twelve.");
+        printf("twelve.");
         break;
-    
+
     case 11:
-        printf("You entered the number eleven.");
+        printf("eleven.");
         break;
-    
+
     case 10:
-        printf("You entered the number ten.");
+        printf("ten.");
         break;
     }
 
@@ -87,15 +89,15 @@ int main(void)
     case 1:
         printf("one.");
         break;
-    
+
     case 2:
         printf("two.");
         break;
-    
+
     case 3:
         printf("three.");
         break;
-    
+
     case 4:
         printf("four.");
         break;
@@ -103,19 +105,19 @@ int main(void)
     case 5:
         printf("five.");
         break;
-    
+
     case 6:
         printf("six.");
         break;
-    
+
     case 7:
         printf("seven.");
         break;
-    
+
     case 8:
         printf("eight.");
         break;
-    
+
     case 9:
         printf("nine.");
         break;
